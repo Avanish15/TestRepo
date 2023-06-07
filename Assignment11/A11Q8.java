7e9a50c6fb2e0c9087155c98f98af953c53dca75
Avanish Ranjan <92219677+Avanish15@users.noreply.github.com>
2023-06-07 17:46:19 +0530

    Add files via upload

---
+++ Assignment11/A11Q8.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // Sort both the arrays first...
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        // Create an array list...
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        // Use two pointers i and j for the two arrays and initialize both with zero.
+        int i = 0, j = 0;
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] < nums2[j]) {
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
+            }
+            else{
+                arr.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        int[] output = new int[arr.size()];
+        int k = 0;
+        while(k < arr.size()){
+            output[k] = arr.get(k);
+            k++;
+        }
+        return output;
+    }
+}
